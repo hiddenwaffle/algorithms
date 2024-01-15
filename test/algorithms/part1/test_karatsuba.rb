d7e863c4d1657237c5d8361780c2c1ab4c6a4d87
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2024-01-15 11:32:50 -0600

    Add challenge problem

--- test/algorithms/part1/test_karatsuba.rb
+++ test/algorithms/part1/test_karatsuba.rb
@@ -7,12 +7,18 @@ class TestKaratsuba < Minitest::Test
   end
 
   def test_rect_int_mult
-    # assert_equal 0, @subject.karatsuba(0, 0)
-    # assert_equal 2, @subject.karatsuba(1, 2)
-    # assert_equal 2, @subject.karatsuba(2, 1)
-    # assert_equal 330, @subject.karatsuba(10, 33)
+    assert_equal 0, @subject.karatsuba(0, 0)
+    assert_equal 2, @subject.karatsuba(1, 2)
+    assert_equal 2, @subject.karatsuba(2, 1)
+    assert_equal 330, @subject.karatsuba(10, 33)
     assert_equal 1000, @subject.karatsuba(10, 100)
-    # assert_equal "7_006_652", @subject.karatsuba(1234, 5678)
-    # assert_equal "-7_006_652", @subject.karatsuba(-1234, 5678)
+    assert_equal 7_006_652, @subject.karatsuba(1234, 5678)
+    assert_equal -7_006_652, @subject.karatsuba(-1234, 5678)
+    # Challenge problem from the website
+    assert_equal 853973422267356706546355086954657447066477805411654748981814531384238681327848992425532501942675408,
+                @subject.karatsuba(
+                  3141592653589793238462643383279502884197169399375105820974944592,
+                  271828182845904523536028747135266249
+                )
   end
 end
