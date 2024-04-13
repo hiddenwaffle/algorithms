8d022fe40607d60ac0d014d41d1dfb82552e35bb
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2024-04-13 15:07:57 -0500

    Next

--- test/algorithms/part1/test_count_inversions.rb
+++ test/algorithms/part1/test_count_inversions.rb
@@ -11,5 +11,9 @@ class TestCountInversions < Minitest::Test
     assert_equal [arr.sort, 0], @subject.sort_and_count_inv(arr)
     arr = [1, 3, 5, 2, 4, 6, 7, 8]
     assert_equal [arr.sort, 3], @subject.sort_and_count_inv(arr)
+    arr = [1, 2, 3, 4, 5, 6, 7, 8]
+    assert_equal [arr.sort, 0], @subject.sort_and_count_inv(arr)
+    arr = [8, 7, 6, 5, 4, 3, 2, 1]
+    assert_equal [arr.sort, 28], @subject.sort_and_count_inv(arr)
   end
 end
