28d07d4073e4b5a9d890bbfe55389ccf23e067af
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2024-04-20 16:43:09 -0500

    Next

--- lib/aoc/2015/3_vacuum.rb
+++ lib/aoc/2015/3_vacuum.rb
@@ -2,10 +2,24 @@ require_relative '../aoc_2015'
 
 class AoC::AoC_2015
   def day_3
-    input = '>'
+    input = File.read(__dir__ + '/3_vacuum.txt')
+    x = 0
+    y = 0
+    houses = ['0x0']
     input.each_char do |c|
-      puts c
+      case c
+      when '^'
+        y -= 1
+      when 'v'
+        y += 1
+      when '>'
+        x += 1
+      when '<'
+        x -= 1
+      end
+      houses << "#{x}x#{y}"
     end
+    puts houses.uniq.count
   end
 end
 
