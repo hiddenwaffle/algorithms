2d17b96c39a8583dcf7ad42158772a608ffb48df
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2024-04-20 16:18:12 -0500

    Next

--- lib/aoc/2015/2_no_math.rb
+++ lib/aoc/2015/2_no_math.rb
@@ -3,15 +3,6 @@ require_relative '../aoc_2015'
 class AoC::AoC_2015
   def day_2
     input = File.read "#{__dir__}/2_no_math.txt"
-
-    def surface_area_and_slack(l, w, h)
-      a = l * w
-      b = w * h
-      c = h * l
-      slack = [a, b, c].min
-      2*a + 2*b + 2*c + slack
-    end
-
     total_sa = 0
     input.lines do |line|
       l, w, h = line.split('x')
@@ -22,6 +13,16 @@ class AoC::AoC_2015
     end
 
     puts total_sa
+  end
+
+  private
+
+  def surface_area_and_slack(l, w, h)
+    a = l * w
+    b = w * h
+    c = h * l
+    slack = [a, b, c].min
+    2*a + 2*b + 2*c + slack
   end
 end
 
--- lib/aoc/2015/2_no_math_part2.rb
+++ lib/aoc/2015/2_no_math_part2.rb
@@ -1,38 +1,40 @@
 require_relative '../aoc_2015'
 
+# width is constant
+#
+# ribbon required is the shortest distance around its sides
+#   a.k.a. smallest perimeter of any one face
+#
+# bow: cubic feet of volume of the present (l*w*h)
+#
+# examples:
+#   2x3x4   requires  2+2+3+3=10 + 2*3*4=24 ====== 34
+#   1x1x10  requires  1+1+1+1=4 + 1*1*10=10 ====== 14
 class AoC::AoC_2015
   def day_2_part_2
     input = File.read "#{__dir__}/2_no_math.txt"
-
-    # width is constant
-    #
-    # ribbon required is the shortest distance around its sides
-    #   a.k.a. smallest perimeter of any one face
-    #
-    # bow: cubic feet of volume of the present (l*w*h)
-    #
-    # examples:
-    #   2x3x4   requires  2+2+3+3=10 + 2*3*4=24 ====== 34
-    #   1x1x10  requires  1+1+1+1=4 + 1*1*10=10 ====== 14
-
-    def surface_area_and_slack(l, w, h)
-      a = l * w
-      b = w * h
-      c = h * l
-      slack = [a, b, c].min
-      2*a + 2*b + 2*c + slack
-    end
-
-    total_sa = 0
+    total_ribbon_len = 0
     input.lines do |line|
       l, w, h = line.split('x')
       l = l.to_i
       w = w.to_i
       h = h.to_i
-      total_sa += surface_area_and_slack(l, w, h)
+      len = ribbon_length(l, w, h)
+      bow_len = bow_ribbon_length(l, w, h)
+      total_ribbon_len += len + bow_len
     end
+    puts total_ribbon_len
+  end
 
-    puts total_sa
+  private
+
+  def ribbon_length(*dim)
+    a, b = dim.sort.take(2)
+    2*a + 2*b
+  end
+
+  def bow_ribbon_length(l, w, h)
+    l*w*h
   end
 end
 
