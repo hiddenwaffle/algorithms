7d785d47cd4fe44b8a7b992de4d4e47aa0fd502f
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2024-04-20 20:35:04 -0500

    Next

--- lib/aoc/2015/4_mining_part2.rb
+++ lib/aoc/2015/4_mining_part2.rb
@@ -1,17 +1,13 @@
 require 'digest'
 require_relative '../aoc_2015'
 
-# Examples
-# Digest::MD5.hexdigest 'abcdef' + '609043'   # => 000001dbbfa3a5c83a2d506429c7b00e
-# Digest::MD5.hexdigest 'pqrstuv' + '1048970' # => 000006136ef2ff3b291c85725f17325c
-
 class AoC::AoC_2015
   def day_4
     secret_key = 'iwrupvqb'
     n = 0
     loop do
       md5 = Digest::MD5.hexdigest("#{secret_key}#{n}")
-      if md5.start_with?('00000')
+      if md5.start_with?('000000')
         puts "#{n} - #{md5}"
         break
       end
