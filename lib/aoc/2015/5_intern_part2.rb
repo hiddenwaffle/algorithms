4aa823c842bda68dc689d3bb51ce2c9daacd39e0
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2024-04-21 14:44:00 -0500

    Next

--- lib/aoc/2015/5_intern_part2.rb
+++ lib/aoc/2015/5_intern_part2.rb
@@ -1,6 +1,8 @@
 require_relative '../aoc_2015'
 
 class AoC::AoC_2015
+  TWO_LETTERS_WITH_ONE_BETWEEN = Regexp.new('([A-Za-z]).\1')
+
   def day_5_part_2
     count = 0
     input = File.read("#{__dir__}/5_intern.input")
@@ -13,7 +15,31 @@ class AoC::AoC_2015
   private
 
   def nice?(str)
-    false # TODO
+    a = two_letters_twice_no_overlap?(str)
+    return unless a
+    b = one_letter_between?(str)
+    return unless b
+    true
+  end
+
+  def two_letters_twice_no_overlap?(str)
+    length = str.length
+    pairs = []
+    (str.length-2).times do |i|
+      pairs << str[i..i+1]
+    end
+    found = false
+    pairs.each do |pair|
+      if str.match /#{pair}.*#{pair}/
+        found = true
+        break
+      end
+    end
+    found
+  end
+
+  def one_letter_between?(str)
+    TWO_LETTERS_WITH_ONE_BETWEEN.match?(str)
   end
 end
 
