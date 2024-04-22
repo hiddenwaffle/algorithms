6f453f9e949833634f4fe4da58f448d9df0cff62
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2024-04-21 23:27:04 -0500

    Next

--- lib/aoc/2015/6_fire.rb
+++ lib/aoc/2015/6_fire.rb
@@ -1,7 +1,53 @@
 require_relative '../aoc_2015'
 
 class AoC::AoC_2015
+  WIDTH = 1000
+  HEIGHT = 1000
+
   def day_6
+    lights = []
+    HEIGHT.times do |row|
+      lights[row] = []
+      WIDTH.times do |col|
+        lights[row][col] = false
+      end
+    end
+    input = File.read(__dir__ + '/6_fire.input')
+    input.lines do |line|
+      command, x1, y1, x2, y2 = parse_line(line)
+      switch(lights, command, y1, x1, y2, x2)
+    end
+    puts lights.flatten.filter { |b| b }.count
+  end
+
+  private
+
+  def parse_line(line)
+    match_data = line.match(/([a-z ]+) (\d+),(\d+) through (\d+),(\d+)/)
+    [
+      match_data[1],
+      match_data[2].to_i,
+      match_data[3].to_i,
+      match_data[4].to_i,
+      match_data[5].to_i
+    ]
+  end
+
+  def switch(lights, command, row1, col1, row2, col2)
+    row_times = row2 - row1 + 1
+    col_times = col2 - col1 + 1
+    row_times.times do |row|
+      col_times.times do |col|
+        case command
+        when 'turn on'
+          lights[row + row1][col + col1] = true
+        when 'turn off'
+          lights[row + row1][col + col1] = false
+        when 'toggle'
+          lights[row + row1][col + col1] = !lights[row + row1][col + col1]
+        end
+      end
+    end
   end
 end
 
