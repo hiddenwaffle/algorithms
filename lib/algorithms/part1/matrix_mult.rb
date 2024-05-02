8bcc8331bea2b76fddd10d047e57bc1a6e27bcd1
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2024-05-02 13:49:25 -0500

    Next

--- lib/algorithms/part1/matrix_mult.rb
+++ lib/algorithms/part1/matrix_mult.rb
@@ -2,7 +2,7 @@
 
 class Algorithms::MatrixMult
   def matrix_mult(a, b)
-    c = Array.new(a.size, a.size)
+    c = Array.new(a.size) { Array.new(a.size) { 0 } }
     # require 'pry'; binding.pry
   end
 end
