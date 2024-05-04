0aa4d1269ece5636b2a2e583aa5599de0371b4e3
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2024-05-03 19:46:16 -0500

    Next

--- lib/algorithms/part1/rec_mat_mult.rb
+++ lib/algorithms/part1/rec_mat_mult.rb
@@ -2,5 +2,41 @@
 
 class Algorithms::RecMatMult
   def rec_mat_mult(x, y)
+    require 'pry'; binding.pry
+  end
+
+  private
+
+  def create_matrix(size)
+    Array.new(size) { Array.new(size)}
+  end
+
+  def extract_col(m, col)
+    size = m.size
+    arr = []
+    size.times do |row|
+      arr << m[row][col]
+    end
+    arr
+  end
+
+  def dot(a, b)
+    raise 'wtf' if a.size != b.size
+    acc = 0
+    a.size.times do |i|
+      acc += a[i] * b[i]
+    end
+    acc
+  end
+
+  def add(a, b)
+    size = a.size
+    z = create_matrix(size)
+    size.times do |row|
+      size.times do |col|
+        z[row][col] = a[row][col] + b[row][col]
+      end
+    end
+    z
   end
 end
