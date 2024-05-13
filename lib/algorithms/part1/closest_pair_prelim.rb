d12add9f48aef74283da89b2e81a24d0b4f5baf5
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2024-05-13 00:07:09 -0500

    Next

---
+++ lib/algorithms/part1/closest_pair_prelim.rb
@@ -0,0 +1,4 @@
+# ClosestPair (Preliminary Version), pg 82
+
+class Algorithms::ClosestPairPrelim
+end
---
+++ test/algorithms/part1/test_closest_pair_prelim.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+require 'algorithms/part1/closest_pair_prelim'
+
+class TestCountInversions < Minitest::Test
+  def setup
+    @subject = Algorithms::ClosestPairPrelim.new
+  end
+end
