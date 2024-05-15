06ca5ec3d6501655f2420b739d8681e7fddcfaf7
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2024-05-14 23:28:01 -0500

    Next

--- lib/algorithms/part1/closest_pair_prelim.rb
+++ lib/algorithms/part1/closest_pair_prelim.rb
@@ -1,4 +1,6 @@
 # ClosestPair (Preliminary Version), pg 82
 
 class Algorithms::ClosestPairPrelim
+  def closest_pair(points)
+  end
 end
--- test/algorithms/part1/test_closest_pair_prelim.rb
+++ test/algorithms/part1/test_closest_pair_prelim.rb
@@ -1,8 +1,13 @@
 require 'test_helper'
 require 'algorithms/part1/closest_pair_prelim'
 
-class TestCountInversions < Minitest::Test
+class TestClosestPairPrelim < Minitest::Test
   def setup
     @subject = Algorithms::ClosestPairPrelim.new
+  end
+
+  def test_closest_pair
+    points = [[1, 8], [2, 5], [4, 7], [6, 3]]
+    @subject.closest_pair(points)
   end
 end
