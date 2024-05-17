5721c3b825c03f4649e8dd0d68eff6b9d7cfc139
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2024-05-17 14:11:46 -0500

    Next

--- lib/aoc/2015/8_matchsticks.rb
+++ lib/aoc/2015/8_matchsticks.rb
@@ -1,7 +1,27 @@
 require_relative '../aoc_2015'
 
+# EXAMPLE = <<~'EOF'
+#   ""
+#   "abc"
+#   "aaa\"aaa"
+#   "\x27"
+# EOF
+
 class AoC::AoC_2015
   def day_8
+    num_codes = 0
+    input = File.read("#{__dir__}/8_matchsticks.input")
+    input.lines.map { |line| line.strip }.each do |line|
+      num_codes += line.size
+    end
+    num_chars = 0
+    input.lines.map { |line| line.strip }.each do |line|
+      # require 'pry'; binding.pry
+      num_chars += eval(line).size
+    end
+    puts "num_codes:  #{num_codes}"
+    puts "num_chars:  #{num_chars}"
+    puts "Final:      #{num_codes - num_chars}"
   end
 end
 
