133551e75ac886620ae227f0adf9b2f56c01026a
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2024-05-17 15:11:50 -0500

    Next

--- lib/aoc/2015/9_single_night.rb
+++ lib/aoc/2015/9_single_night.rb
@@ -1,8 +1,14 @@
 require_relative '../aoc_2015'
 
+EXAMPLE = <<~EOF
+  London to Dublin = 464
+  London to Belfast = 518
+  Dublin to Belfast = 141
+EOF
+
 class AoC::AoC_2015
   def day_9
-    input = File.read("#{__dir__}/9_single_night.input")
+    input = EXAMPLE # File.read("#{__dir__}/9_single_night.input")
   end
 end
 
