d4c101e3cab189f8454acdda187107b55603446a
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2024-05-19 12:35:44 -0500

    Next

--- lib/aoc/2015/11_corporate.rb
+++ lib/aoc/2015/11_corporate.rb
@@ -4,7 +4,6 @@ class AoC::AoC_2015
   def day_11
     str = 'zw'
     10.times do |i|
-      break
       str = inc(str)
       puts str
     end
@@ -26,6 +25,18 @@ class AoC::AoC_2015
     else
       "#{str[..-2]}#{(last.ord + 1).chr}"
     end
+  end
+
+  def validate(str)
+    # 1) Must include one increasing straight of at least three letters
+    #   Examples: abc, bcd, cde, and so on, up to xyz
+    # TODO: Loop x=0 to length-3, with range x..(x+2)
+    # 2) May not contain the letters i, o, or l
+    return false if str.includes?('i')
+    return false if str.includes?('o')
+    return false if str.includes?('l')
+    # 3) Must contain at least two different, non-overlapping pairs of letters
+    #   Examples: aa, bb, or zz
   end
 end
 
