89cdce1eb0389ab54f1e9fbdd9b92f7f1a59d05a
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2024-05-19 14:27:06 -0500

    Next

--- lib/aoc/2015/11_corporate.rb
+++ lib/aoc/2015/11_corporate.rb
@@ -30,13 +30,30 @@ class AoC::AoC_2015
   def validate(str)
     # 1) Must include one increasing straight of at least three letters
     #   Examples: abc, bcd, cde, and so on, up to xyz
-    # TODO: Loop x=0 to length-3, with range x..(x+2)
+    return false unless has_straight(str)
     # 2) May not contain the letters i, o, or l
+    return false unless contains_iol(str)
+    # 3) Must contain at least two different, non-overlapping pairs of letters
+    #   Examples: aa, bb, or zz
+  end
+
+  def has_straight(str)
+    ords = str.chars.map(&:ord)
+    found = false
+    (ords.length-2).times do |i|
+      if ords[i+1] == ords[i]+1 && ords[i+2] == ords[i]+2
+        found = true
+        break
+      end
+    end
+    found
+  end
+
+  def contains_iol(str)
     return false if str.includes?('i')
     return false if str.includes?('o')
     return false if str.includes?('l')
-    # 3) Must contain at least two different, non-overlapping pairs of letters
-    #   Examples: aa, bb, or zz
+    true
   end
 end
 
