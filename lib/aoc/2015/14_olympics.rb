b35b54be6c9c28061573189a459e10edc9e21bdb
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2024-05-22 23:32:06 -0500

    Next

--- lib/aoc/2015/14_olympics.rb
+++ lib/aoc/2015/14_olympics.rb
@@ -1,5 +1,10 @@
 require_relative '../aoc_2015'
 
+EXAMPLE = <<~EOF
+  Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
+  Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.
+EOF
+
 class AoC::AoC_2015
   def day_14
   end
