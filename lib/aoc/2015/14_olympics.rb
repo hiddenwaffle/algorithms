fe5fa429bb6fb1b0fc174fedfbc2fd4f68dd98b0
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2024-05-23 07:29:42 -0500

    Next

--- lib/aoc/2015/14_olympics.rb
+++ lib/aoc/2015/14_olympics.rb
@@ -1,12 +1,25 @@
 require_relative '../aoc_2015'
 
+DISTANCE = 1120
 EXAMPLE = <<~EOF
   Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
   Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.
 EOF
 
+Racer = Data.define(:speed, :stamina, :recovery)
+
 class AoC::AoC_2015
   def day_14
+    input = EXAMPLE
+    racers = parse_racers(input)
+  end
+
+  private
+
+  def parse_racers(input)
+    input.lines.map do |line|
+      binding.pry
+    end
   end
 end
 
