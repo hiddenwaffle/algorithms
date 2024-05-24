ab88a3b93a4667204f56a629947f8dca642e053f
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2024-05-23 19:35:40 -0500

    Next

--- lib/aoc/2015/15_hungry.rb
+++ lib/aoc/2015/15_hungry.rb
@@ -1,5 +1,10 @@
 require_relative '../aoc_2015'
 
+EXAMPLE = <<~EOF
+  Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8
+  Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3
+EOF
+
 class AoC::AoC_2015
   def day_15
   end
