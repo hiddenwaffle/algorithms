955bff562ec16d7582052bdf16a3d529f671bb4c
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2024-05-26 17:56:42 -0500

    Next

--- lib/aoc/2015/19_rudolph.rb
+++ lib/aoc/2015/19_rudolph.rb
@@ -1,7 +1,19 @@
 require_relative '../aoc_2015'
 
+EXAMPLE_MOLECULE = 'HOH'
+EXAMPLE = <<~EOF
+  H => HO
+  H => OH
+  O => HH
+EOF
+
+MOLECULE = 'CRnCaSiRnBSiRnFArTiBPTiTiBFArPBCaSiThSiRnTiBPBPMgArCaSiRnTiMgArCaSiThCaSiRnFArRnSiRnFArTiTiBFArCaCaSiRnSiThCaCaSiRnMgArFYSiRnFYCaFArSiThCaSiThPBPTiMgArCaPRnSiAlArPBCaCaSiRnFYSiThCaRnFArArCaCaSiRnPBSiRnFArMgYCaCaCaCaSiThCaCaSiAlArCaCaSiRnPBSiAlArBCaCaCaCaSiThCaPBSiThPBPBCaSiRnFYFArSiThCaSiRnFArBCaCaSiRnFYFArSiThCaPBSiThCaSiRnPMgArRnFArPTiBCaPRnFArCaCaCaCaSiRnCaCaSiRnFYFArFArBCaSiThFArThSiThSiRnTiRnPMgArFArCaSiThCaPBCaSiRnBFArCaCaPRnCaCaPMgArSiRnFYFArCaSiThRnPBPMgAr'
+INPUT = File.read "#{__dir__}/19_rudolph.input"
+
 class AoC::AoC_2015
   def day_19
+    molecule = EXAMPLE_MOLECULE
+    input = EXAMPLE
   end
 end
 
