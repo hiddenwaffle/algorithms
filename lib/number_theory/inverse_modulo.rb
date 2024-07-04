e4079b0757c446b38a2796eacfdd0edc1a650778
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2024-07-04 08:56:21 -0500

    Next

--- lib/number_theory/inverse_modulo.rb
+++ lib/number_theory/inverse_modulo.rb
@@ -14,4 +14,11 @@ end
 
 
 # puts NumberTheory::InverseModulo.new.inverse_modulo(5, 7) # 3
-puts NumberTheory::InverseModulo.new.inverse_modulo(9, 31)
+# puts NumberTheory::InverseModulo.new.inverse_modulo(9, 31) # 7
+# puts NumberTheory::InverseModulo.new.inverse_modulo(27, 392) # 363
+# puts NumberTheory::InverseModulo.new.inverse_modulo(18, 19) # 18
+# puts NumberTheory::InverseModulo.new.inverse_modulo(5, 10) # infinite loop
+# puts NumberTheory::InverseModulo.new.inverse_modulo(17, 19) # 9
+# puts NumberTheory::InverseModulo.new.inverse_modulo(35, 3) # 2
+# puts NumberTheory::InverseModulo.new.inverse_modulo(21, 5) # 1
+puts NumberTheory::InverseModulo.new.inverse_modulo(15, 7) # 1
