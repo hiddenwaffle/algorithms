6925680bf181a3e2728a0e28ab2229a4942f8aaf
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2024-07-05 08:43:46 -0500

    Next

--- lib/number_theory/inverse_modulo.rb
+++ lib/number_theory/inverse_modulo.rb
@@ -3,10 +3,11 @@ require_relative '../number_theory'
 
 class NumberTheory::InverseModulo
   # a \equiv b \pmod m
-  def inverse_modulo(d, m)
+  def inverse_modulo(b, m)
     i = 1
     loop do
-      return i / d if i % d == 0
+      puts "Trying: #{i}"
+      return i / b if i % b == 0
       i += m
     end
   end
@@ -23,4 +24,10 @@ end
 # puts NumberTheory::InverseModulo.new.inverse_modulo(21, 5) # 1
 # puts NumberTheory::InverseModulo.new.inverse_modulo(15, 7) # 1
 # puts NumberTheory::InverseModulo.new.inverse_modulo(20, 3) # 2
-puts NumberTheory::InverseModulo.new.inverse_modulo(20, 3)
+# puts NumberTheory::InverseModulo.new.inverse_modulo(42, 5)
+
+puts NumberTheory::InverseModulo.new.inverse_modulo(50388, 11)
+# puts NumberTheory::InverseModulo.new.inverse_modulo(46189, 12)
+# puts NumberTheory::InverseModulo.new.inverse_modulo(42636, 13)
+# puts NumberTheory::InverseModulo.new.inverse_modulo(32604, 17)
+# puts NumberTheory::InverseModulo.new.inverse_modulo(29172, 19)
